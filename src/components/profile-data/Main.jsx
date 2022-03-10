a72a410f29eb9abcf3d28e6e1ca250115c531f37
unknown <lamica.olivares@gmail.com>
2022-03-10 10:40:07 -0300

    inter

--- src/components/profile-data/Main.jsx
+++ src/components/profile-data/Main.jsx
@@ -1,41 +1,41 @@
 import React from 'react'
 import BasicData from './BasicData';
 import { useState } from "react";
-import { db } from "../../firebaseConfig/firebase";
-import { collection, addDoc, doc , deleteDoc } from "firebase/firestore";
+import { addData  } from "../../firebaseConfig/firebase.js";
+//  import { collection, addDoc } from "firebase/firestore";
 import { ShowEvents } from '../events-data/ShowEvents';
 
 
 const Main = () => {
     const [profileData, setProfileData] = useState({});
     const [eventData, setEventData] = useState({});
-   
-    const addData = async (e) => {
-        e.preventDefault();
-        console.log("funcionando addData");
-        try {
-            const docRef = await addDoc(collection(db, 'dates'), {
-                profile: {...profileData},
-                events :{...eventData}
+    const agregarDatos = () => {addData(setProfileData)}
+    // const addData = async (e) => {
+    //     e.preventDefault();
+    //     console.log("funcionando addData");
+    //     try {
+    //         const docRef = await addDoc(collection(db, 'dates'), {
+    //             profile: {...profileData},
+    //             events :{...eventData}
             
                 
-            });
-            console.log('Document written with ID: ', docRef.id);
-        } catch (e) {
-            console.error('Error adding document: ', e);
-        }
-        setProfileData({});
+    //         });
+    //         console.log('Document written with ID: ', docRef.id);
+    //     } catch (e) {
+    //         console.error('Error adding document: ', e);
+    //     }
+    //     setProfileData({});
 
-        const deleteDoc = async (id) => {
-           await deleteDoc(doc(db, 'dates' , id));
-        }
+    //     // const eliminar = async (id) => {
+    //     //    await deleteDoc(doc(db, 'dates' , id));
+    //     // }
 
 
-    };
+    
     return (
         <main>
             <pre>{JSON.stringify(profileData, null , 2)}</pre>
-            <button  onClick={addData}>agregar</button>
+            <button  onClick={agregarDatos}>agregar</button>
             <div >
                   <BasicData
         setProfileData={setProfileData}
--- src/firebaseConfig/firebase.js
+++ src/firebaseConfig/firebase.js
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+import { collection, addDoc, doc , deleteDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA25iI-eWjyZ2t-wqldNAbEYrcMMtRtlOI",
@@ -15,3 +15,26 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
+
+ export const addData = async (e , setProfileData) => {
+  e.preventDefault();
+  console.log("funcionando addData");
+  try {
+      const docRef = await addDoc(collection(db, 'dates'), {
+          profile: {...profileData},
+          events :{...eventData}
+      
+          
+      });
+      console.log('Document written with ID: ', docRef.id);
+  } catch (e) {
+      console.error('Error adding document: ', e);
+  }
+  setProfileData({});
+}
+
+// const deleteDoc = async (id) => {
+//   await deleteDoc(doc(db, 'dates' , id));
+// }
+
+
